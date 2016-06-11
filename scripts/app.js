3f196d21635c99b6ec16b983f4b8c6c5ce1be3bb
Khalid Sookia <khalidsookia@gmail.com>
2016-06-11 17:16:52 +0200

    Did all routes with controllers

--- scripts/app.js
+++ scripts/app.js
@@ -8,11 +8,61 @@ app.config(function($routeProvider) {
     .when('/', {
         controller: 'AppCtrl',
         templateUrl: 'templates/home.html'
+    }).when('/about', {
+        controller: 'AboutCtrl',
+        templateUrl: 'templates/about.html'
+    }).when('/contact', {
+        controller: 'ContactCtrl',
+        templateUrl: 'templates/contact.html'
+    }).when('/friends', {
+        controller: 'FriendsCtrl',
+        templateUrl: 'templates/friends.html'
+    }).when('/quotation', {
+        controller: 'QuotationCtrl',
+        templateUrl: 'templates/quotation.html'
+    }).when('/recruitement', {
+        controller: 'RecruitementCtrl',
+        templateUrl: 'templates/recruitement.html'
+    }).when('/test', {
+        controller: 'TestCtrl',
+        templateUrl: 'templates/test.html'
+    }).when('/error/:code', {
+        controller: 'ErrorCtrl',
+        templateUrl: 'templates/error.html'
     }).otherwise({
-        redirectTo: '404'
+        redirectTo: '/error/404'
     });
 });
 
+app.controller('ErrorCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+    console.log('in ErrorCtrl');
+    console.log('Error code: ' + $routeParams.code);
+}]);
+
 app.controller('AppCtrl', ['$scope', function ($scope) {
     console.log('in AppCtrl');
-}])
+}]);
+
+app.controller('AboutCtrl', ['$scope', function ($scope) {
+    console.log('in AboutCtrl');
+}]);
+
+app.controller('ContactCtrl', ['$scope', function ($scope) {
+    console.log('in ContactCtrl');
+}]);
+
+app.controller('FriendsCtrl', ['$scope', function ($scope) {
+    console.log('in FriendsCtrl');
+}]);
+
+app.controller('QuotationCtrl', ['$scope', function ($scope) {
+    console.log('in QuotationCtrl');
+}]);
+
+app.controller('RecruitementCtrl', ['$scope', function ($scope) {
+    console.log('in RecruitementCtrl');
+}]);
+
+app.controller('TestCtrl', ['$scope', function ($scope) {
+    console.log('in TestCtrl');
+}]);
