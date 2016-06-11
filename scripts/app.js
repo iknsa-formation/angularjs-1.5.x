998e5f4e2296144b7ef7eefd67d445fb4a79f65e
Khalid Sookia <khalidsookia@gmail.com>
2016-06-11 17:02:06 +0200

    Initialised ng app, injected ngRoute and created first route

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
 /**
  * @author Khalid Sookia <khalidsookia@gmail.com>
  */
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        controller: 'AppCtrl',
+        templateUrl: 'templates/home.html'
+    }).otherwise({
+        redirectTo: '404'
+    });
+});
+
+app.controller('AppCtrl', ['$scope', function ($scope) {
+    console.log('in AppCtrl');
+}])
