896b107524852938179991ca1a0380ec4ea19399
Khalid Sookia <khalidsookia@gmail.com>
2016-06-13 22:52:31 +0200

    Added a directive to override the default email validator

--- scripts/app.js
+++ scripts/app.js
@@ -35,6 +35,22 @@ app.directive('maDirective', function() {
     }
 });
 
+app.directive('realEmail', function() {
+    var regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+    return {
+     require: '?ngModel',
+     // https://docs.angularjs.org/api/ng/service/$compile#-link-
+     link: function(scope, iElement, iAttrs, controller) {
+        if (controller && controller.$validators.email) {
+                controller.$validators.email = function(modelValue) {
+                return regex.test(modelValue);
+                };
+            }
+        }
+    };
+});
+
 app.controller('ErrorCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
     console.log('in ErrorCtrl');
     console.log('Error code: ' + $routeParams.code);
