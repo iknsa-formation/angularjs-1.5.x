79ac524084b69a246ead6904963bf6fbb63e1df9
Khalid Sookia <khalidsookia@gmail.com>
2016-06-14 00:13:13 +0200

    Added list method of contacts

--- scripts/controllers/ContactCtrl.js
+++ scripts/controllers/ContactCtrl.js
@@ -1,14 +1,14 @@
 app.controller('ContactCtrl', ['$scope', 'ContactFactory', function ($scope, ContactFactory) {
     console.log('in ContactCtrl');
-    console.log(ContactFactory.contacts);
+    console.log(ContactFactory.list());
 
     $scope.imgName = 'violet';
     $scope.pageTitle = 'Vous êtes sur la page de contact';
 
     $scope.new = function(contact) {
 
-        ContactFactory.new(contact);
-        console.log(ContactFactory.contacts);
+        ContactFactory.create(contact);
+        console.log(ContactFactory.list());
 
         $scope.contact = {};
         $scope.form.$setPristine();
