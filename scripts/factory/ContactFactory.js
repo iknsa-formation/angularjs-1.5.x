54beecedaa48598c5443de5a401eb327c6c86e36
Khalid Sookia <khalidsookia@gmail.com>
2016-06-14 00:15:25 +0200

    Added contactFactory and initialised some defaults contacts

---
+++ scripts/factory/ContactFactory.js
@@ -0,0 +1,28 @@
+app.factory('ContactFactory', [function () {
+    console.log('in ContactFactory');
+
+    var ContactFactory = {};
+
+    ContactFactory.contacts = {
+        'khalid.sookia@iknsa.com': {
+            name: 'Khalid Sookia',
+            email: 'khalid.sookia@iknsa.com',
+            message: 'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            tnc: false
+        },
+        'arsene.lupin@leblanc.com': {
+            name: 'Arsène Lupin',
+            email: 'arsene.lupin@leblanc.com',
+            message: 'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            tnc: true
+        },
+        'clark@superman.com': {
+            name: 'Clark Kent',
+            email: 'clark@superman.com',
+            message: 'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            tnc: false
+        },
+    };
+
+    return ContactFactory;
+}])
