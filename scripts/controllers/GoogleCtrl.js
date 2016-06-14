5eda16857b3684ce909a096dfe89d4e783013d45
Khalid Sookia <khalidsookia@gmail.com>
2016-06-14 10:16:22 +0200

    Testing http service

--- scripts/controllers/GoogleCtrl.js
+++ scripts/controllers/GoogleCtrl.js
@@ -1,4 +1,6 @@
 app.controller('GoogleCtrl', ['$scope', 'GoogleService', function ($scope, GoogleService) {
     var google = GoogleService;
     console.log(google);
+
+    $scope.data = google;
 }])
--- scripts/factory/GetGoogleService.js
+++ scripts/factory/GetGoogleService.js
@@ -1,11 +1,13 @@
 app.factory('GoogleService', ['$http', function ($http) {
     this.getGoogle = $http({
       method: 'GET',
-      url: 'https://www.google.fr/'
+      url: 'http://localhost:3000/#/'
     })
     .then(function successCallback(response) {
         console.log('success');
-        console.log(response);
+        console.log(response.data);
+
+        return response.data;
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
